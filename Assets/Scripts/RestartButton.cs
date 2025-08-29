510a45ecd30a860c54a166f716d6323c952d1911
mathew <mathew.tran.dev@gmail.com>
2025-08-29 09:56:32 +0800

    use SFX for restart button at beginning of game

--- Assets/Scripts/RestartButton.cs
+++ Assets/Scripts/RestartButton.cs
@@ -1,10 +1,23 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RestartButton : MonoBehaviour
 {
+    public Button mButton;
+    public AudioSource mAudioSource;
     public void RestartGame()
     {
+        mButton.enabled = false;
+        mAudioSource.Play();
+        StartCoroutine(WaitForSoundToFinish());
+    }
+
+    public IEnumerator WaitForSoundToFinish()
+    {
+        yield return new WaitUntil(() => mAudioSource.isPlaying == false);
         SceneManager.LoadScene("Scene1");
     }
 }
